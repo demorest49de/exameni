9a0e08208e2c8ddba891b82ded84925fe02297a4
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-25 11:09:00 +0300

    Changes index.tsx tmp.js

--- archive/tmp.js
+++ archive/tmp.js
@@ -328,3 +328,129 @@ ReactDOM.render(
 // Что надо написать вместо xxx, чтобы код работал оптимально?
 
 
+
+
+
+import
+  React, {useState, MouseEvent, ChangeEvent}
+  from
+    
+    'react'
+  ;
+import
+  ReactDOM
+  from
+    
+    'react-dom'
+  ;
+import
+  
+  './index.css'
+  ;
+function
+
+User
+(
+)
+{
+  
+  const
+    [userName, setUserName] = useState<string>(
+      ""
+    )
+  
+  return    (
+    <div>
+      <p>{userName}</p>
+      <input
+        value={userName}
+        onChange={(e) => setUserName(e.currentTarget.value)}
+      />
+    </div>
+  )
+}
+ReactDOM.render(
+  <User/>,
+  document
+    .getElementById(
+      'root'
+    )
+);
+// Что надо написать вместо ххх, чтобы инпут был контролируемым?
+
+
+
+
+
+
+
+
+
+import
+  React, {useState, MouseEvent, ChangeEvent}
+  from
+    
+    'react'
+  ;
+import
+  ReactDOM
+  from
+    
+    'react-dom'
+  ;
+import
+  
+  './index.css'
+  ;
+
+function Notes() {
+  
+  const [newNote, setNewNote] = useState<string>(
+    ""
+  )
+  
+  const [notes, setNotes]
+    = useState<Array<string>>([])
+  
+  const onChangeHandler = (
+    e: ChangeEvent<HTMLTextAreaElement>
+  ) => setNewNote(e.currentTarget.value)
+  
+  const addNote = () => {
+    setNotes([newNote, ...notes])
+    setNewNote(
+      ""
+    )
+  }
+  
+  return (
+    <div>
+            <textarea
+              value={newNote}
+              onChange={onChangeHandler}
+              onBlur={addNote}
+            />
+      <div>
+        <button
+          // onClick={() => setNewNote('')}
+        >Clear notes list
+        </button>
+      </div>
+      <h4>Notes:</h4>
+      <div>
+        {notes.map(n => <p>{n}</p>)}
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Notes/>,
+  document
+    .getElementById(
+      'root'
+    )
+);
+// Что надо написать вместо ххх,
+
+// чтобы при клике список заметок очищался?
