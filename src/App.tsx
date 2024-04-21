22f965d4e0a6bc3e39c522abbc541aed03647203
Shevchenko Andrey <demorest49de@gmail.com>
2024-04-22 00:48:26 +0300

    Changes .gitignore App.tsx glubokoe-kopirovanie.js index.css index.tsx package.json tmp.js yarn.lock

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+           return (
+        <div className="App">
+        </div>
+    );
+}
+
+// import React  from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import {App} from './App';
+//
+//
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
